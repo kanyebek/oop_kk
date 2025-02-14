3b741429c336f8fcbcba9acc054f97dc1c373d5b
kanyebek <koshonovkanybek@gmail.com>
2025-02-14 14:06:30 +0600

    HW:Did HW4

---
+++ homeworks/hw4.py
@@ -0,0 +1,36 @@
+# 1)
+def logger(func):
+
+    def wrapper(*args, **kwargs):
+        print(f"Вызов функции: {func.__name__} с аргументами: {args}")
+        func(*args, **kwargs)
+    return wrapper
+
+@logger
+def greet(name):
+    print(f"Привет, {name}!")
+
+
+greet("Алиса")
+
+# 2)
+
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __add__(self, other):
+        return  Rectangle(self.width + other.width, self.height + other.height )
+
+    def __str__(self, ):
+        return f"Прямоугольник {self.width}x{self.height}"
+
+
+rect1 = Rectangle(3, 4)
+rect2 = Rectangle(2, 5)
+rect3 = rect1 + rect2
+print(rect1)
+print(rect2)
+print(rect3)
